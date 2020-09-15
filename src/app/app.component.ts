21b4d0d1a89115aad06b5bd341b0eb940d7ee2a6
Lucas <l.alves001@gmail.com>
2020-09-15 15:39:51 -0300

    Exibindo dados no componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: String = "My tasks";
+
+  constructor() {
+    this.todos.push('Terminar o curso de Angular');
+    this.todos.push('Terminar o curso de .Net');
+    this.todos.push('Iniciar as aulas de Clean Architecture Flutterando');
+  }
+
 }
