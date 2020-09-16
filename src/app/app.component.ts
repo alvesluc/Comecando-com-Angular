e0b56610c9dca12b46751622ba03024431fbafe3
Lucas <l.alves001@gmail.com>
2020-09-15 22:55:57 -0300

    Crianção e validação de formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Todo } from "src/models/todo.model";
 
 @Component({
@@ -10,7 +11,18 @@ export class AppComponent {
   public todos: Todo[] = [];
   public title: String = "My tasks";
 
-  constructor() {
+  public form: FormGroup;
+
+
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
     this.todos.push(new Todo(1, 'Terminar o curso de Angular', false));
     this.todos.push(new Todo(2, 'Terminar o curso de .Net', false));
     this.todos.push(new Todo(3, 'Iniciar as aulas de Clean Architecture Flutterando', false));
@@ -25,11 +37,11 @@ export class AppComponent {
   };
 
   markAsDone(todo: Todo) {
-
+    todo.done = true;
   };
 
   markAsUndone(todo: Todo) {
-
+    todo.done = false;
   };
 
 }
